jvanheesch/transactional-rollback-poc

add example demonstrating default @Transactional rollback behavior (= rollback only for unchecked exceptions)

src/main/java/com/github/jvanheesch/Application.java
@@ -1,0 +1,11 @@
+package com.github.jvanheesch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

src/main/java/com/github/jvanheesch/NoteRepository.java
@@ -1,0 +1,8 @@
+package com.github.jvanheesch;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}

src/main/java/com/github/jvanheesch/NoteService.java
@@ -1,0 +1,32 @@
+package com.github.jvanheesch;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @Transactional
+    public Note saveCheckedException(Note note) throws MyCheckedException {
+        noteRepository.save(note);
+        throw new MyCheckedException();
+    }
+
+    @Transactional
+    public Note saveUncheckedException(Note note) {
+        noteRepository.save(note);
+        throw new MyUncheckedException();
+    }
+
+    static class MyCheckedException extends Exception {
+    }
+
+    static class MyUncheckedException extends RuntimeException {
+    }
+
+}

jvanheesch/transactional-rollback-example

src/main/java/io/github/jvanheesch/Application.java
@@ -1,0 +1,11 @@
+package io.github.jvanheesch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

src/main/java/io/github/jvanheesch/NoteRepository.java
@@ -1,0 +1,8 @@
+package io.github.jvanheesch;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}

src/main/java/io/github/jvanheesch/NoteService.java
@@ -1,0 +1,31 @@
+package io.github.jvanheesch;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @Transactional
+    public Note saveCheckedException(Note note) throws MyCheckedException {
+        noteRepository.save(note);
+        throw new MyCheckedException();
+    }
+
+    @Transactional
+    public Note saveUncheckedException(Note note) {
+        noteRepository.save(note);
+        throw new MyUncheckedException();
+    }
+
+    static class MyCheckedException extends Exception {
+    }
+
+    static class MyUncheckedException extends RuntimeException {
+    }
+}